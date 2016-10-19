b7771517a738c7c46dcd8a6aa407d969168a8045
anbuchelva <anbuchelva@gmail.com>
2016-10-20 00:23:36 +0530

    build librmnetctl

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -11,3 +11,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/data/qmi_config.xml:system/etc/data/qmi_config.xml \
     $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml
 endif
+
+# Connectivity Engine support 
+PRODUCT_PACKAGES += \ 
+    libcnefeatureconfig \ 
+    librmnetctl 
